0755eb018eb075597e5433a172f46cff14697285
web-aQ <web-aQ@users.noreply.github.com>
2021-03-10 10:43:14 +0800

    '搭建项目'

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../views/Login/index.vue'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    redirect:"login"
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/Login/index.vue')
+  },
+  
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
